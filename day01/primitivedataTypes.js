c0c94f4cb7dad9cdbbcf7c06573db3225225e723
Harikaran <49835256+Harikaran2204@users.noreply.github.com>
2025-08-20 12:44:13 +0530

    Add files via upload

---
+++ day01/primitivedataTypes.js
@@ -0,0 +1,26 @@
+var city = "Villupuram"
+console.log("I live in city "+city)
+var city = "Chennai"
+console.log("I live in city "+city)
+console.log("The data type of city is "+typeof city)
+
+const marks = 85
+console.log(marks+10)
+console.log("The data type of marks is "+typeof marks)
+
+let isWeekend = true
+isWeekend = false
+
+if(isWeekend = true){
+
+        console.log("Relax")
+}
+
+else{
+
+        console.log("Work")
+}
+
+var futureGoal
+console.log(typeof futureGoal)
+
---
+++ day01/variables.js
@@ -0,0 +1,11 @@
+var firstName = "Hari Karan"
+var companyName = "Neurealm (Formerly GAVS)"
+var mobileNumber = +917708388323
+var isAutomation = true
+var hasPlaywright
+
+console.log(firstName+"---->"+typeof firstName)
+console.log(companyName+"---->"+typeof companyName)
+console.log(mobileNumber+"---->"+typeof mobileNumber)
+console.log(isAutomation+"---->"+typeof isAutomation)
+console.log(hasPlaywright+"---->"+typeof hasPlaywright)
